7ff63fcb304eec834501db2537a31e3eb52f3d61
suminashrestha <suminastha77@gmail.com>
2024-01-07 22:52:57 +0545

    setTimeout function

---
+++ setTimeout.js
@@ -0,0 +1,16 @@
+// setTimeout() = function in JS that allows you to schedule
+// the execution  of a function after an amount of TimeRanges(milliseconds0)
+
+// setTimeout(callback,delay);
+
+//This is callback method:
+function sayHello(){
+    window.alert("Hello through callback method");
+}
+setTimeout(sayHello,3000);
+
+//This is anonymous function:
+setTimeout(function(){window.alert("Hello through anonymous function")},5000);
+
+//This is arrow function:
+setTimeout(()=>window.alert("Hello through arrow function"),7000);
