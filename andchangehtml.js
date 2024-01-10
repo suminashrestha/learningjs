dbd5aa8146a0a2849c1f7332864bde320266aed5
suminashrestha <suminastha77@gmail.com>
2024-01-10 21:46:00 +0545

    10JAN

---
+++ andchangehtml.js
@@ -0,0 +1,21 @@
+//examle 1 <h1>
+//Step1 Create the element
+const newH1 = document.createElement("h1");
+
+
+//Step 2 Add Attribute/Properties
+newH1.textContent = "I like pizza.";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+
+//Step 3 Append element to DOM
+// document.body.append(newH1);
+// document.body.prepend(newH1);
+//  document.getElementById("box4").prepend(newH1);
+ 
+
+//Remove the element
+ document.body.removeChild(newLink);
+ document.getElementById("box1").removeChild(newLink);
+
+
--- dom.js
+++
@@ -1,4 +0,0 @@
-//DOM = DOCUMENT OBJECT MODEL
-    // object{} that represents the page you see in the web browser
-    // and provides you with API to interact with it.
-    
